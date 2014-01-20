4c387ef6d20ff83752cad3290b415006edfac440
Anjan Nepal <rockanjan@gmail.com>
2014-01-20 15:43:57 -0500

    modified bash script: if no output directories exist, create them

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if[ ! -d out/model]; then
+    mkdir -p out/model;
+fi
+if[ ! -d out/decoded]; then
+    mkdir -p out/decoded;
+fi
 export PATH=/home/anjan/jdk/bin:$PATH
 ant clean build
-java -Xmx15G -Dfile.encoding=UTF-8 -classpath bin:lib/mallet.jar program.Main
+java -Xmx1G -Dfile.encoding=UTF-8 -classpath bin:lib/mallet.jar program.Main
